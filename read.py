2dd56adefc5d951059d45e72f192def61b6f067b
MH <mhhsu6110@gmail.com>
2021-06-20 16:12:41 +0800

    counting total good and show 1st good

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		counts += 1
-		if counts % 1000 == 0:
+		if counts % 10000 == 0:
 			print(len(data))
 	print(len(data))
 print('file read done, total ', len(data), 'data')
@@ -23,3 +23,10 @@ print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+#	print(good)
+print('total', len(d), 'message include good')
+print(good[0])
