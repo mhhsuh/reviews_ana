6487f12f6e6491ea1e3798599945f974cad0f012
MH <mhhsu6110@gmail.com>
2021-06-20 15:48:17 +0800

    calc avg length

--- read.py
+++ read.py
@@ -1,13 +1,19 @@
 data = []
-count = 0
+counts = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
-		#count += 1
-		if count % 1000 == 0:
+		counts += 1
+		if counts % 1000 == 0:
 			print(len(data))
-		count += 1
+	print(len(data))
+print('file read done, total ', len(data), 'data')
 
-print(data[0])
-print('-----------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('average is', sum_len/len(data))
+
+
+
