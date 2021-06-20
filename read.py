7726dbe1beca4256496f93408121cb9b03877195
MH <mhhsu6110@gmail.com>
2021-06-20 15:57:07 +0800

    add filter func in reviews

--- read.py
+++ read.py
@@ -15,5 +15,11 @@ for d in data:
 
 print('average is', sum_len/len(data))
 
-
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
 
